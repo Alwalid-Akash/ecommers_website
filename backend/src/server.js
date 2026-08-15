3e4267e4cbc80c0b2d78504d5b18eeb1e6276b25
MD AL WALID <alwalidakash@gmail.com>
2026-08-15 22:39:07 +0200

    create server and connect database

---
+++ backend/src/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const cors = require("cors");
+require("dotenv").config();
+
+console.log("Database:", process.env.DB_NAME);
+
+
+const pool = require("./config/db");
+
+
+const app = express();
+
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});

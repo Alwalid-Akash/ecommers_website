2682b5ceb750ebf36096b71608f8a1de2f97c9bb
MD AL WALID <alwalidakash@gmail.com>
2026-09-23 22:46:55 +0200

    deploy in vercer edit render url

--- backend/src/server.js
+++ backend/src/server.js
@@ -46,6 +46,6 @@ app.use(errorHandler);
 
 // -------------------- START SERVER --------------------
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
